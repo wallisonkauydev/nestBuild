afaf595da9a4883f7ceb631b72d45e67d5079264
Jheovanny <jheovanny06@hotmail.com>
2023-10-05 20:34:44 -0300

    deploy

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-  });
   await app.listen(3000);
 }
 bootstrap();
